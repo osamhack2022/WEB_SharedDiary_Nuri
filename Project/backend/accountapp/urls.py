0fae0e825c71a936c2983d7d3ec5f613d6f168ce
Aiden-Kwak <67510613+Aiden-Kwak@users.noreply.github.com>
2022-09-17 11:39:27 +0000

    error fixed & django-extensions for development

--- Project/backend/accountapp/urls.py
+++ Project/backend/accountapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import RegistrationAPIView, LoginAPIView
+from .views import RegistrationAPIView, LoginAPIView, UserRetrieveUpdateAPIView
 
 
 urlpatterns = [
--- Project/backend/backend/settings.py
+++ Project/backend/backend/settings.py
@@ -57,14 +57,14 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'rest_framework',
     'accountapp',
+
+    # for development
+    'django_extensions',
 ]
 
 REST_FRAMEWORK = {
     'EXCEPTION_HANDLER': 'core.exceptions.core_exception_handler',
-    'NON_FIELD_ERRORS_KEY': 'error',
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'authentication.backends.JWTAuthentication',
-    )
+    'NON_FIELD_ERRORS_KEY': 'error'
 }
 
 MIDDLEWARE = [

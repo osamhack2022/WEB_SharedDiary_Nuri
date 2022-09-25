2e5c25c4599d70414219b11fdb5cafd853ff98eb
Aiden-Kwak <67510613+Aiden-Kwak@users.noreply.github.com>
2022-09-25 15:08:40 +0000

    react router test

--- Project/backend/backend/settings.py
+++ Project/backend/backend/settings.py
@@ -82,7 +82,8 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-CORS_ORIGIN_WHITELIST = ('http://127.0.0.1:3000', 'http://localhost:3000')
+# CORS_ORIGIN_WHITELIST = ('http://127.0.0.1:3000', 'http://localhost:3000')
+CORS_ORIGIN_ALLOW_ALL = True # 개발용
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = "backend.urls"
--- Project/frontend/src/App.jsx
+++ Project/frontend/src/App.jsx
@@ -1,10 +1,49 @@
 import './App.css';
+import { BrowserRouter, Routes, Route, Link, Navigate, useNavigate, useLocation } from 'react-router-dom';
 import {Signup} from './account';
+
+function Header() {
+  return (
+    <div class="container">
+      <nav class="navbar">
+        <a><Link to="/">Home</Link></a>{" "}
+        <a><Link to="/accounts/signup">Sign Up</Link></a>{" "}
+        <a><Link to="/accounts/login">Log In</Link></a>
+      </nav>
+    </div>
+  );
+}
+
+function RandingPage() {
+  return (
+    <div className='App'>
+      RandingPage
+    </div>
+  );
+}
+
+function NotFound() {
+  return (
+    <div>
+      <p>Page Not Found</p>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <Signup/>
+      <div className="container">
+        <BrowserRouter>
+          <Header/>
+          <Routes>
+            <Route path="/" element={<RandingPage/>}/>
+            <Route path="/accounts/signup" element={<Signup/>}/>
+            <Route path="*" element={<NotFound/>}/>
+          </Routes>
+        </BrowserRouter>
+        
+      </div>
     </div>
   );
 }

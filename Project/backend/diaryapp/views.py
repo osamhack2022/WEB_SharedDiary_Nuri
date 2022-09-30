cebdd504967eba821f9aacffa9264cc0d662c664
Mingi Jung <wjd4813@naver.com>
2022-09-30 14:05:42 +0000

    diaryapp commit/mingi

--- Project/backend/diaryapp/views.py
+++ Project/backend/diaryapp/views.py
@@ -1,3 +1,23 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-
 # Create your views here.
+from diaryapp.models import Diary
+from django.views.generic import CreateView, WriteView, DetailView, UpdateView, DeleteView
+
+
+class DiaryListView():
+
+class DiaryCreateView(CreateView):
+    model = User
+    
+class DirayWriteView(WriteView):
+    model = User
+
+class DirayDetailView(DetailView):
+    model = User
+
+class DiaryUpdateView(UpdateView):
+    model = User
+
+class DiaryDeleteView(DeleteView):
+    model = User

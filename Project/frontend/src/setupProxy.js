f55e4e64e8690f1834feb084ba9554728dc62c31
Aiden-Kwak <67510613+Aiden-Kwak@users.noreply.github.com>
2022-10-07 10:26:40 +0000

    setupProxy url 그대로넣음

--- Project/frontend/src/setupProxy.js
+++ Project/frontend/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = (app) => {
   app.use(
     createProxyMiddleware("/accounts", {
-      target: "http://localhost:8000",
+      target: "https://osamhack2022-web-shareddiary-nuri-g67v9pv5qvvcpg4q-3000.githubpreview.dev",
       changeOrigin: true,
     }),
   );

5949cdebb3dfb761868ee55ff2339c01da4d5366
Mingi Jung <wjd4813@naver.com>
2022-10-10 13:27:05 +0000

    diary frontend

--- Project/frontend/src/_components/Diary.jsx
+++ Project/frontend/src/_components/Diary.jsx
@@ -9,22 +9,46 @@ import {
 } from "react-icons/bs";
 export { Diary };
 
-function Diary() {
+// {
+//     "content":res.data[0].content,
+//     "created_at":res.data[0].created_at,OOO
+//     "id":res.data[0].id,
+//     "image":res.data[0].image,
+//     "title":res.data[0].title,
+//     "to_open":res.data[0].to_open,OOO
+//     "updated_at":res.data[0].updated_at,OOO
+//      writer
+//     프로필 이미지 필요
+//   }
+function Diary({testData}) {
     return (
         <div className="Diary">
             <div className='card'>
-                <div className='diary-profile'>
+                <div className='diary-profile' style={{backgroundColor:"white"}}>
                     <div className='diary-profile-img'>
-                        <img src={dev_img_profile} alt="profile-img" />
+                        {/* <img src={testData[0].image} alt="profile-img" /> */}
                     </div>
-                    <div className='diary-profile-info'>
-                        <p>닉네임</p>
-                        <p style={{fontSize:".9rem", color:"#536471"}}>@duckracoon</p>
-                    </div> 
+                    <div className='diary-detail'>
+                        <div className='diary-profile-info'>
+                            {/* <h4>{testData.nickname}</h4> */}
+                            {/* <p style={{fontSize:".9rem", color:"#536471"}}>{testData[0].username}</p>
+                            <p style={{fontSize:".9rem", color:"#536471"}}>{testData[0].created_at}</p>
+                            <p style={{fontSize:".9rem", color:"#536471"}}>{testData[0].updated_at}</p>
+                            <p style={{fontSize:".9rem", color:"#536471"}}>{testData[0].to_open}</p> */}
+                        </div>
+                        <div className="diary-img">
+                            {/* <img src={testData[0].image} alt="diary-img" /> */}
+                        </div>
+                    </div>
                 </div>
-                <p>2022년 3월 30일</p>
-                <div className="diary-img">
-                    <img src={dev_img_diary} alt="diary-img" />
+            </div>
+        </div>
+    );
+}
+
+
+/* <div className="diary-data">
+                    <p>2022년 3월 30일</p>
                 </div>
                 <div className='diary-content'>
                     <div className='diary-content-p'>
@@ -35,7 +59,6 @@ function Diary() {
                         <BsStar/>
                     </div>
                 </div>
-                <p>일기 확인하기</p>
                 <div className='diary-reaction'>
                     <div className='icon diary-reaction-likes'>
                         <BsHeart/>
@@ -49,8 +72,4 @@ function Diary() {
                         <BsEye/>
                         <p>59</p>
                     </div>
-                </div>
-            </div>
-        </div>
-    );
-}
+                </div> */
--- Project/frontend/src/randingpage/RandingPage.jsx
+++ Project/frontend/src/randingpage/RandingPage.jsx
@@ -1,19 +1,39 @@
 import './randingpage.css'
 import { Diary } from '../_components';
 export { RandingPage };
+import dev_img_profile from '../static/img/dev_test/프로필이미지.png';
+import dev_img_diary from '../static/img/dev_test/doge-goorm.jpg';
+import axios from "axios";
 
-function RandingPage() {
-    return (
-      <div className='RandingPage'>
-        <div className="RandingPage-container">
-          <div className="diary-area randingpage-area">
-            <Diary/>
-            <Diary/>
-          </div>
-          <div className="recommend-area randingpage-area" style={{backgroundColor:"red"}}>
-              추천일기장 구역
-          </div>
+function RandingPage(props) {
+  let data;
+  axios.get('/home/diary')
+        .then(function(res) {
+          //res.data[1].title
+          data=res.data
+          const getEntries = Object.entries(res.data[0]).map((entrie, idx) => {
+            return console.log(entrie, idx);
+          });
+          console.log(getEntries)
+          } 
+        )
+        .catch(error=>console.log(error))
+
+  // const content = data[0].content
+  // console.log(content)
+
+  return (
+    <div className='RandingPage'>
+      <div className="RandingPage-container">
+        <div className="diary-area randingpage-area">
+          
+          <Diary/>
+        </div>
+        <div className="recommend-area randingpage-area" style={{backgroundColor:"red"}}>
+            추천일기장 구역
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
+  

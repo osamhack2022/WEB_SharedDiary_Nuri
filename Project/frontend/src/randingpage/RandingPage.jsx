684df8e65334f4da80abc99d76ba84ca0d1bcdad
Mingi Jung <wjd4813@naver.com>
2022-10-14 14:08:53 +0000

    useEffect적용, diary데이터, state에 저장됨, redux필요

--- Project/frontend/src/randingpage/RandingPage.jsx
+++ Project/frontend/src/randingpage/RandingPage.jsx
@@ -4,29 +4,23 @@ export { RandingPage };
 import dev_img_profile from '../static/img/dev_test/프로필이미지.png';
 import dev_img_diary from '../static/img/dev_test/doge-goorm.jpg';
 import axios from "axios";
-
-function RandingPage(props) {
-  let data;
-  axios.get('/home/diary')
-        .then(function(res) {
-          //res.data[1].title
-          data=res.data
-          const getEntries = Object.entries(res.data[0]).map((entrie, idx) => {
-            return console.log(entrie, idx);
-          });
-          console.log(getEntries)
-          } 
-        )
-        .catch(error=>console.log(error))
-
-  // const content = data[0].content
-  // console.log(content)
+import React, { useState, useEffect } from 'react';
+ 
+export default function RandingPage(props) {
+  const [variable, setVariable] = useState();
+  useEffect(() => {    
+    axios.get('/home/diary')
+    .then(res => {
+      setVariable(res.data);
+    })
+  .catch(error=>console.log(error))
+  }, []);
+  console.log(variable)
 
   return (
     <div className='RandingPage'>
       <div className="RandingPage-container">
         <div className="diary-area randingpage-area">
-          
           <Diary/>
         </div>
         <div className="recommend-area randingpage-area" style={{backgroundColor:"red"}}>

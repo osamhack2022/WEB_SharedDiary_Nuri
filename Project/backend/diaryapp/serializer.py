b9a51c4682715dff7e4171b07d7a108308ac6f43
Aiden-Kwak <67510613+Aiden-Kwak@users.noreply.github.com>
2022-10-16 06:23:28 +0000

    diary redux연결, 데이서 렌더링 완료

--- Project/backend/diaryapp/serializer.py
+++ Project/backend/diaryapp/serializer.py
@@ -2,6 +2,7 @@ from .models import Diary, Note
 from rest_framework import serializers
 
 class DiarySerializer(serializers.ModelSerializer):
+    note = serializers.CharField(max_length=45, read_only=True)
     class Meta:
         model = Diary
         fields = '__all__'

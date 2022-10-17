200646ef54f3702cf4ab54d517dba90360c87269
Aiden-Kwak <67510613+Aiden-Kwak@users.noreply.github.com>
2022-10-17 13:53:29 +0000

    Quill editor added to diaryCreate

--- Project/frontend/src/diary/diaryCreate.jsx
+++ Project/frontend/src/diary/diaryCreate.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { useLocation} from 'react-router-dom';
 import axios from "axios";
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 export {DiaryCreate}
 
 function DiaryCreate(){
@@ -47,12 +49,56 @@ function DiaryCreate(){
             });
     }
 
+    // Quil 설정
+    const toolbarOptions = [
+        ["link", "image", "video"],
+        [{ header: [1, 2, 3, false] }],
+        ["bold", "italic", "underline", "strike"],
+        ["blockquote"],
+        [{ list: "ordered" }, { list: "bullet" }],
+        [{ color: [] }, { background: [] }],
+        [{ align: [] }],
+    ]; 
+    const formats = [
+        "header",
+        "font",
+        "size",
+        "bold",
+        "italic",
+        "underline",
+        "strike",
+        "align",
+        "blockquote",
+        "list",
+        "bullet",
+        "indent",
+        "background",
+        "color",
+        "link",
+        "image",
+        "video",
+        "width",
+    ];
+    const modules = {
+        toolbar: {
+          container: toolbarOptions,
+        },
+    };
+
+
     return (
         <div className="DiaryCreate">
             일기장안에서 일기생성
             <p><input type="text" name="title" value={title} onChange={onChange} placeholder="title"/></p>
-            <p><textarea type="text" name="content" onChange={onChange}>{content}</textarea></p>
+            <div className='quill-textarea' style={{padding:"5rem"}}>
+                <ReactQuill theme="snow" value={content} onChange={onChange} modules={modules} formats={formats}
+                    style={{height:"450px"}}
+                />
+            </div>
+            {/* <p><textarea type="text" name="content" onChange={onChange}>{content}</textarea></p> */}
             <button onClick={onSubmit}>button</button>
+            
+            
         </div>
     );
 }

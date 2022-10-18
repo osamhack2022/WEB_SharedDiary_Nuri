d9f1942f642bfc58ff2e50679e3df2ba947e6b54
Aiden-Kwak <67510613+Aiden-Kwak@users.noreply.github.com>
2022-10-18 14:44:49 +0000

    로그인 css완료(2)

--- Project/frontend/src/account/Login.jsx
+++ Project/frontend/src/account/Login.jsx
@@ -77,11 +77,11 @@ function Login() {
                     <svg width="40" height="40" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox='0 0 48 48'>
                         <path d="M7.7 29.5c-2.3 0-4.1-.8-5.6-2.4C.7 25.5 0 23 0 20c0-4.5 1.1-8.4 3.4-11.6C5.7 5.2 9.2 2.6 14 .5l3.1 7c-3 .5-5.6 1.5-7.8 3A10 10 0 0 0 5 15.9c.7-.3 1.7-.5 2.8-.5 2.2 0 4 .7 5.3 2 1.3 1.3 2 3 2 5.1 0 2.1-.7 3.8-2 5.1a7.3 7.3 0 0 1-5.4 2Zm19 0c-2.3 0-4.2-.8-5.6-2.4-1.4-1.6-2.1-4-2.1-7.1 0-4.5 1-8.4 3.4-11.6C24.7 5.2 28 2.6 32.8.5l3.2 7c-3 .5-5.7 1.5-7.9 3a10 10 0 0 0-4.2 5.3c.8-.3 1.7-.5 2.8-.5 2.2 0 4 .7 5.3 2 1.4 1.3 2 3 2 5.1 0 2.1-.6 3.8-2 5.1a7.3 7.3 0 0 1-5.3 2Z" fill="#4CA771"/>
                     </svg>
-                    <p style={{color: '#808080'}}><strong>누리, 사색을 공유하다</strong></p>
+                    <p style={{color: '#808080'}}>누리, 사색을 공유하다</p>
                 </div>
                 <div className='login-form'>
-                    <p className='input'><input type="text" name="email" value={email} onChange={onChange} placeholder="email"/></p>
-                    <p className='input'><input type="password" name="password" value={password} onChange={onChange} placeholder="password"/></p>   
+                    <p className='input'><input type="text" name="email" value={email} onChange={onChange} placeholder="이메일"/></p>
+                    <p className='input'><input type="password" name="password" value={password} onChange={onChange} placeholder="비밀번호"/></p>   
                     <p className='submit'><input type="submit" value="로그인" onClick={onSubmit}/></p>
                 </div>
             </div>
